551b4dc90b0066d2229d374f2087ad8311cecb2b
lutfiandri <lutfiandri232@gmail.com>
2020-11-20 22:15:51 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+        container: {
+            center: true,
+            padding: "2rem",
+        },
+        fontFamily: {
+            sans: "Patrick Hand",
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
